90ce36572eb60e99a7ac555d36b5355c92cff353
Reisa Sitohang <reisa.sitohang@bestseller.com>
2017-07-20 11:40:39 +0200

    added dirty jquery include

--- js/viewLoader.js
+++ js/viewLoader.js
@@ -1,19 +1,24 @@
 
 // Loads the header view into the main page
-(function(){
-    var header = document.querySelector('header');
-    var myRequest = new Request('navigation.html');
-    fetch(myRequest).then(function(response) {
-        return response.text().then(function(text) {
-            header.innerHTML = text;
-        });
-    });
+// (function(){
+//     var header = document.querySelector('header');
+//     var myRequest = new Request('navigation.html');
+//     fetch(myRequest).then(function(response) {
+//         return response.text().then(function(text) {
+//             header.innerHTML = text;
+//         });
+//     });
 
-    var footer = document.querySelector('footer');
-    myRequest = new Request('footer.html');
-    fetch(myRequest).then(function(response) {
-        return response.text().then(function(text) {
-            footer.innerHTML = text;
-        });
-    });
-})();
+//     var footer = document.querySelector('footer');
+//     myRequest = new Request('footer.html');
+//     fetch(myRequest).then(function(response) {
+//         return response.text().then(function(text) {
+//             footer.innerHTML = text;
+//         });
+//     });
+// })();
+
+$(document).ready(function() {
+    $('#header-nav').load('./navigation.html');
+    $('#footer').load('/footer.html');
+});
